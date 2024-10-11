e5d5197b6797fd6354fe56e38c5528c789a59380
Modassir22 <modsseirmallick@gmail.com>
2024-10-12 00:51:49 +0530

    Search in Sorted Array

---
+++ searchInSortedTwoDArray.java
@@ -0,0 +1,29 @@
+public class searchInSortedTwoDArray {
+    
+public static boolean stairCaseSearch(int matrix[][],int key){
+    int row = 0;
+    int col = matrix[0].length-1;
+
+    while(row <= matrix.length-1 && col >= 0){
+           if(matrix[row][col] == key){
+            System.out.println("Key is at Index ("+row+","+col+")");
+            return true;
+           }else if(key > matrix[row][col]){
+            row++;
+           }else{
+            col--;
+           }
+    }
+    System.out.println("Key not found");
+    return false;
+}
+    public static void main(String args[]){
+        int matrix[][] = {{10,20,30,40},
+                         {15,25,35,45},
+                         {27,29,37,48},
+                         {32,33,39,50}};
+            int key = 33;             
+
+        System.out.println(stairCaseSearch(matrix,key));
+    }
+}
